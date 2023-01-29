c10d168f46a8e9c6f652b260e4c60e0047374a0f
mike clemens <deathcow@gmail.com>
2023-01-29 02:26:18 +0000

    Updated test screens

--- src/main.rs
+++ src/main.rs
@@ -270,59 +270,37 @@ impl Display {
     }
 }
 
+// Generate random 22 byte strings to show on display.
 fn random_stringz() -> String {
     let mut rng = rand::thread_rng();
-    (0..20)
+    (0..22)
         .map(|_| (rng.gen_range(32, 128) as u8) as char)
         .collect()
 }
 
 fn main() {
+    // Take a handle to our I2c display object
     let mut small_oled_display = Display::new();
-    sleep(Duration::from_millis(1000));
+
+    sleep(Duration::from_millis(100));
     small_oled_display.init_display();
-    sleep(Duration::from_millis(1000));
-    small_oled_display.clear_screen(0x00);
+    sleep(Duration::from_millis(100));
+    small_oled_display.clear_screen(0xAA);
 
+    // display random chars at random rows with random invert
+    let mut rng = rand::thread_rng();
+    let mut random_row: u8 = rng.gen_range(0, 8);
+    let mut random_inv: u8 = rng.gen_range(2, 6);
+    let mut random_string = random_stringz();
+    let mut random_slice: &[u8] = random_string.as_bytes();
     loop {
-        let mut rng = rand::thread_rng();
-        let mut random_row: u8 = rng.gen_range(0, 8);
-        let mut random_inv: u8 = rng.gen_range(2, 6);
-
-        let mut random_string = random_stringz();
-        let mut random_slice: &[u8] = random_string.as_bytes();
-
         small_oled_display.display_text_18x8(random_row as u8, 0, random_inv % 2, random_slice);
-
         random_string = random_stringz();
         random_slice = random_string.as_bytes();
         random_row = rng.gen_range(0, 8);
-        random_inv = rng.gen_range(2, 6);
-        small_oled_display.display_text_18x8(random_row as u8, 0, random_inv % 2, random_slice);
+        random_inv = rng.gen_range(1, 20);
 
-        random_string = random_stringz();
-        random_slice = random_string.as_bytes();
-        random_row = rng.gen_range(0, 8);
-        random_inv = rng.gen_range(2, 6);
-        small_oled_display.display_text_18x8(random_row as u8, 0, random_inv % 2, random_slice);
-
-        random_string = random_stringz();
-        random_slice = random_string.as_bytes();
-        random_row = rng.gen_range(0, 8);
-        random_inv = rng.gen_range(2, 6);
-        small_oled_display.display_text_18x8(random_row as u8, 0, random_inv % 2, random_slice);
-
-        random_string = random_stringz();
-        random_slice = random_string.as_bytes();
-        random_row = rng.gen_range(0, 8);
-        random_inv = rng.gen_range(2, 6);
-        small_oled_display.display_text_18x8(random_row as u8, 0, random_inv % 2, random_slice);
-
-        random_string = random_stringz();
-        random_slice = random_string.as_bytes();
-        random_row = rng.gen_range(0, 8);
-        random_inv = rng.gen_range(2, 6);
-        small_oled_display.display_text_18x8(random_row as u8, 0, random_inv % 2, random_slice);
+        // dump cache to console so we can verify cache works
 
         // println!("{}", "------------------------");
         // println!();
